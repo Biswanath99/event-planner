6b8988c363addeb6ce2950377540b30a153c201a
Biswanath Banik <banikb1999@gmail.com>
2026-05-31 22:31:09 +0530

    Updated

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Backend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\{Appointment,Contact};
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('backend.index');
+         $booking   = Appointment::count();    
+         $inquiries = Contact::count();    
+         return view('backend.index',compact('booking','inquiries'));
     }
 }
--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -11,8 +11,17 @@
                     <div class="col-lg-3 col-md-6 col-12 mb-4">
                         <div class="card shadow-md bg-white border border-success mb-3">
                             <div class="card-body">
-                                <span class="fw-semibold d-block mb-1 text-dark">Total Categories</span>
-                                <h3 class="card-title mb-2">0</h3>
+                                <span class="fw-semibold d-block mb-1 text-dark">Total Appointments</span>
+                                <h3 class="card-title mb-2">{{$booking}}</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-12 mb-4">
+                        <div class="card shadow-md bg-white border border-success mb-3">
+                            <div class="card-body">
+                                <span class="fw-semibold d-block mb-1 text-dark">Total Inquiries</span>
+                                <h3 class="card-title mb-2">{{$inquiries}}</h3>
                             </div>
                         </div>
                     </div>
